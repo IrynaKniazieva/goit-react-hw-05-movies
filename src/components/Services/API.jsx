553d4d60e6af54d5494aa2f986602e46febb32f2
IrynaKniazieva <gleb0812@gmail.com>
2022-11-21 01:11:43 +0200

    start

---
+++ src/components/Services/API.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+// const API_KEY = "8009629a1fea118cf1b1e37731fe7687";
+// axios.defaults.baseURL = "https://api.themoviedb.org"
+
+// export const fetchMovies = page => {
+//     return axios('trending/movie/day', {
+//       params: {
+//         api_key: API_KEY,
+//         page,
+//       },
+//     });
+//   };
+
+
+// получить коллекцию для главной страницы /trending/get-trending 
+
+// export const getTrending = async () => {
+//     try {
+//         const response = await fetch ()
+//     }catch (error) {
+
+//     }
+     
+// }
