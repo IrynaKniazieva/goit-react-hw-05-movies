261311d51e5ed60acf3f56a55940c33ef4194e8d
IrynaKniazieva <gleb0812@gmail.com>
2023-01-15 01:13:34 +0200

    api

--- src/components/Services/API.jsx
+++ src/components/Services/API.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-// const API_KEY = "8009629a1fea118cf1b1e37731fe7687"
+
+
 axios.defaults.baseURL = "https://api.themoviedb.org/3"
 axios.defaults.params = {
   api_key: '8009629a1fea118cf1b1e37731fe7687',
