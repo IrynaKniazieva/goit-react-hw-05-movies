ff30912c440df5dddb707391a6f1e6c56319abf7
IrynaKniazieva <gleb0812@gmail.com>
2023-01-16 18:38:42 +0200

    in process

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes, } from "react-router-dom";
 import { Layout } from "components/Layout/Layout";
 import { NotFound } from "pages/NotFound/NotFound";
-
+import { Home } from "pages/Home/Home";
 
 
 
@@ -9,7 +9,7 @@ export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>Home</div>}/>
+        <Route index element={<Home />}/>
         <Route path="movies" element={<div>Movies</div>}/>
         <Route path="movies/:movieId" element={<div>Detals</div>}>
           <Route path="cast" element={<div>Cast</div>}/>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,29 +3,36 @@ import { TrendingList } from "components/TrendingList/TrendingList";
 import { getTrendingMovies } from "../../components/Services/API"
 import { Container, Title} from '../Home/Home.styled';
 
-export const Home = () => {
-  const [trendingMovies, setTrendingMovies] = useState([]);
+// export const Home = () => {
+//   const [trendingMovies, setTrendingMovies] = useState([]);
 
-  useEffect(() => {
-    const renderTrendingMovies = async () => {
-        try {
-            const trendingMovies = await getTrendingMovies();
-            setTrendingMovies(trendingMovies);
-        } catch (error) {
-            console.log(error)
-        }
-    };
-    renderTrendingMovies();
-},[]);
+//   useEffect(() => {
+//     const renderTrendingMovies = async () => {
+//         try {
+//             const trendingMovies = await getTrendingMovies();
+//             setTrendingMovies(trendingMovies);
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     };
+//     renderTrendingMovies();
+// },[]);
   
   
-    return (
-      <main>
-        <Container>
-          <Title>Trending today</Title>
-        <TrendingList movies={trendingMovies}/>
-        </Container>
+//     return (
+//       <main>
+//         <Container>
+//           <Title>Trending today</Title>
+//         <TrendingList movies={trendingMovies}/>
+//         </Container>
         
-      </main>
-    );
-  };
+//       </main>
+//     );
+//   };
+
+
+export const Home = () => {
+  return 
+    <h1>Trending today</h1>
+  
+}
