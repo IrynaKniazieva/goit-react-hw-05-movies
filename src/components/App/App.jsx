cf390fc994e29f9ad9e908f7dcd8806d41b1852b
IrynaKniazieva <gleb0812@gmail.com>
2023-01-17 15:00:12 +0200

    in process

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />}/>
         <Route path="movies" element={<div>Movies</div>}/>
-        <Route path="movies/:movieId" element={<div>Detals</div>}>
+        <Route path="movies/:movieId" element={<div>Detals про кіно кіно</div>}>
           <Route path="cast" element={<div>Cast</div>}/>
           <Route path="reviews" element={<div>Reviews</div>}/>
         </Route>
---
+++ src/components/Services/API.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+axios.defaults.params = {
+  api_key: '8009629a1fea118cf1b1e37731fe7687',
+};
+// первый вариант
+// список самых популярных фильмов на сегодня для создания коллекции на главной странице.
+// export const getTrendingMovies = async() => {
+//     const response = await axios.get(`/trending/movie/day`);
+//     return response.data.results;
+//   };
+
+
+// вариант как в уроке
+export async function getTrendingMovies() {
+  const response = await axios.get(`trending/movie/day`);
+  return response.data;
+}
