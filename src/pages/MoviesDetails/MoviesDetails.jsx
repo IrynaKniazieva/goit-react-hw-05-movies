2a2dc2ed20893edca74f0d2cc366783c9a3a7e8d
IrynaKniazieva <gleb0812@gmail.com>
2023-01-19 00:40:31 +0200

    Update MoviesDetails.jsx

--- src/pages/MoviesDetails/MoviesDetails.jsx
+++ src/pages/MoviesDetails/MoviesDetails.jsx
@@ -13,15 +13,25 @@ export const MoviesDetails = () => {
 
 console.log(details);
 
+const date = () => new Date(`${details.release_date}`).getFullYear();
+const userScore = () => Math.round(`${details.vote_average}`);
+const genres = () => details.genres.map(genre => genre.name).join(', ');
+
+
+
 return (
 <>
 {details && (
   <div>
-    <h2>{details.title}{details.release_date}</h2>
+    <button>Go back</button>
+    <h2>{details.title} {date()} </h2>
+    <p>User Score: {userScore()}%</p>
     <h3>Overview</h3>
     <p>{details.overview}</p>
+    <h3>Genres</h3>
+    <p>{genres()}</p>
    
-    <img src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`} alt={details.title}></img>
+    <img src={`https://image.tmdb.org/t/p/w500/${details.poster_path}`} alt={details.title} width="300"></img>
   </div>
 )}
 </>
